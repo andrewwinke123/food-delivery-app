7e9dc2249d303012f12d29eace0ac917cad838de
andrewwinke123 <andrewwinke@gmail.com>
2023-12-06 17:07:16 -0500

    adding in the initial link

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,5 +1,23 @@
 import React from 'react'
+import classes from './header.module.css'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
-  return <div>Header</div>
+
+  const user = {
+    name: 'John',
+  }
+
+  const cart = {
+    totalCount: 10,
+  }
+
+
+  return <header className={classes.header}>
+    <div className={classes.container}>
+      <Link to= '/' className={classes.logo}>
+        FOOD!!!
+      </Link>
+    </div>
+  </header>
 }
