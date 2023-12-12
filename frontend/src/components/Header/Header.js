04a8d4d96254eab2877cfe47fc93783c238f30b7
andrewwinke123 <andrewwinke@gmail.com>
2023-12-12 16:54:35 -0500

    setting up the nav links

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -3,7 +3,6 @@ import classes from './header.module.css'
 import { Link } from 'react-router-dom'
 
 export default function Header() {
-
   const user = {
     name: 'John',
   }
@@ -12,12 +11,37 @@ export default function Header() {
     totalCount: 10,
   }
 
+  const logout = () => {}
 
-  return <header className={classes.header}>
-    <div className={classes.container}>
-      <Link to= '/' className={classes.logo}>
-        FOOD!!!
-      </Link>
-    </div>
-  </header>
+  return (
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <Link to='/' className={classes.logo}>
+          Order Time
+        </Link>
+        <nav>
+          <ul>
+            {user ? (
+              <li className={classes.menu_container}>
+                <Link to='/profile'>{user.name}</Link>
+                <div className={classes.menu}>
+                  <Link to='/profile'>Profile</Link>
+                  <Link to='/orders'>Orders</Link>
+                  <a onClick={logout}>Logout</a>
+                </div>
+              </li> 
+            ) : (
+              <Link to='/login'>Login</Link>
+            )}
+
+            <li>
+              <Link to='/cart'>
+                Cart {cart.totalCount > 0 && <span>{cart.totalCount}</span>}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
 }
