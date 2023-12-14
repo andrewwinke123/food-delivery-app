90bf2cd561af199eb19f65ea94f612f8654a82a6
andrewwinke123 <andrewwinke@gmail.com>
2023-12-14 16:47:19 -0500

    setting up reducer

--- frontend/src/pages/Home/HomePage.js
+++ frontend/src/pages/Home/HomePage.js
@@ -1,5 +1,18 @@
 import React from 'react'
 
+const initialState = {foods: []}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'FOODS_LOADED':
+      return { ...state, foods: action.payload }
+      default:
+        return state
+  }
+}
+
 export default function HomePage() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { foods } = state
   return <div>HomePage</div>
 }
