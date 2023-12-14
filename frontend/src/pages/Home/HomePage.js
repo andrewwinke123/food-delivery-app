4afd02f9ae52e9b82caa54b6f4abce3d27693c8a
andrewwinke123 <andrewwinke@gmail.com>
2023-12-14 17:21:07 -0500

    loading the foods

--- frontend/src/pages/Home/HomePage.js
+++ frontend/src/pages/Home/HomePage.js
@@ -1,18 +1,25 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react'
 
-const initialState = {foods: []}
+import { getAll } from '../../services/foodService.js'
+
+const initialState = { foods: [] }
 
 const reducer = (state, action) => {
   switch(action.type) {
     case 'FOODS_LOADED':
       return { ...state, foods: action.payload }
-      default:
-        return state
+    default:
+      return state
   }
 }
 
 export default function HomePage() {
   const [state, dispatch] = useReducer(reducer, initialState)
   const { foods } = state
+
+  useEffect(() => {
+    getAll().then(foods => dispatch({ type: 'FOODS_LOADED', payload: foods }))
+  }, [])
+
   return <div>HomePage</div>
 }
