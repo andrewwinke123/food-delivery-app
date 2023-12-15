338019404ac4393c2df25f2b7ec74b99cf8a889d
andrewwinke123 <andrewwinke@gmail.com>
2023-12-14 21:02:42 -0500

    adding photos dynamically

--- frontend/src/components/Thumbnails/Thumbnails.js
+++ frontend/src/components/Thumbnails/Thumbnails.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import classes from './Thumbnails.module.css'
+
 
 export default function Thumbnails({ foods }) {
   return (
-    <div>Thumbnails{foods.length}</div>
+    <ul className={classes.list}>
+      {foods.map(food => {
+      return (
+        <li key={food.id}>
+          <Link to={`/foods/${food.id}`}>
+            <img 
+              className={classes.image}
+              src={`/foods/${food.imageUrl}`} 
+              alt={food.name}
+            />
+          </Link>
+        </li>
+      )
+    })}
+    </ul>
   )
 }
